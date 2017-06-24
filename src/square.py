a7da23fda28a560fb0e74a35547a5a40a6512eeb
marekmroz <m2mroz@hotmail.com>
2017-06-24 17:47:06 +0100

    add more docs

--- src/square.py
+++ src/square.py
@@ -1,5 +1,9 @@
 def my_square(x):
-    """ Takes a value and returns the squared value."""
+    """
+    Takes a value and returns the squared value.
+    
+    Some more documentation...
+    """
     return x*x
 
 my_square(3)
